1094ed1c8be8d71c6e65ecccf1a12d27be5a6ff7
Josh <joshuajmv@me.com>
2023-05-19 12:44:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 
 
 $(document).ready(function(){
-  
+
 	const panels = document.querySelectorAll('.panel');
 
-	panels.forEach(panel => {
-	  panel.addEventListener('click', () => {
-		// Remove .show class from all .panel elements
-		panels.forEach(p => p.classList.remove('show'));
-		
-		// Toggle .show class on clicked .panel
-		panel.classList.toggle('show');
-	  });
-	});
-	
 
-});
+
+    panels.forEach(panel => {
+      panel.addEventListener('click', () => {
+        // Check if the clicked panel has the .show class
+        if (panel.classList.contains('show')) {
+          // If it does, remove the .show class
+          panel.classList.remove('show');
+        } else {
+          // If it doesn't, remove .show class from all .panel elements
+          panels.forEach(p => p.classList.remove('show'));
+          // Add .show class to clicked .panel
+          panel.classList.add('show');
+        }
+      });
+    });
+
+	});
+
+
